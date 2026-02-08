58b7677ad5f35479e1252805df8e6252ef38c38e
Maleklabbaci <Marcolabbaci@gmail.com>
2026-02-08 06:09:45 +0100

    fix: Update website URL to ivision.agency
    The primary website URL was outdated and has been updated to reflect the new domain. This ensures users are directed to the correct online presence.

--- constants.ts
+++ constants.ts
@@ -14,7 +14,7 @@ export const SOCIAL_LINKS = {
   linkedin: "", // Pas encore
   facebook: "https://www.facebook.com/agencyivision/",
   tiktok: "", // Pas encore
-  website: "https://website-i-vision.vercel.app/",
+  website: "https://ivision.agency/",
 };
 
 // Logo URL
